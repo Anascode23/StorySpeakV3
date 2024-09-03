22ca181322e13d721a524aaee378f3bd00fbb2bc
anascode23 <anas.ali@ecutbildning.se>
2024-09-03 15:23:14 +0100

    Implementing MP3 player for the audiobook. HTML part done

---
+++ StorySpeakV3/Controllers/Mp3Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StorySpeakV3.Controllers
+{
+    public class Mp3Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
